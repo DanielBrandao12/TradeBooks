018f0ab6653350849ea42fce5ccf84312c6e02e3
DanielBrandao12 <drb12da@gmail.com>
2022-12-27 17:40:41 -0300

    criação do multer e model books

--- controllers/myStore.js
+++ controllers/myStore.js
@@ -1,10 +1,58 @@
-
+const database = require("../database/models");
 
 function myStore(req, res) {
   
   res.render('myStore' )
 
 };
+/*
+Ainda falta inserir dois inputs de quantidade e de preço
+criar o arquivo file 
+*/
+function addBook(req, res){
+  let id = req.session.userLogged.id
+  const{
+    title_book,
+    category,
+    tipo,
+    description,
+    autor,
+    editora,
+    ano_edicao,
+    numero_edicao,
+    acabamento,
+    idioma,
+    numero_paginas,
+    condicao_livro,
+    tipo_anuncio,
+    preco,
+    image,
+    quantidade,
+
+  } = req.body
+
+  database.Books.create({
+    USER_ID: id,
+    TITLE_BOOK: title_book,
+    CATEGORY: category,
+    TIPO: tipo,
+    DESCRIPTION_BOOK: description,
+    AUTOR: autor,
+    EDITORA: editora,
+    ANO_EDICAO: ano_edicao,
+    NUMERO_EDICAO: numero_edicao,
+    ACABAMENTO:acabamento,
+    IDIOMA: idioma,
+    NUMERO_PAGINAS: numero_paginas,
+    condicao_book: condicao_livro,
+    tipo_anuncio: tipo_anuncio,
+    PRICE: preco,
+    galeria_fotos: image,
+    QTDE:quantidade,
+  }).then((data) => {
+    return res.json(data)
+  })
+}
 
 
 module.exports = { myStore };
--- routes/myStore.js
+++ routes/myStore.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require("../controllers/myStore")
-
+const notLoggedUserMiddleware = require('../middlewares/notLoggedUserMiddleware')
 
 /* GET home page. */
-router.get('/', controller.myStore);
+router.get('/',notLoggedUserMiddleware, controller.myStore);
 
 
 module.exports = router;
