1e6142adea52ccdef7a457367cf6fc17decb6f27
DanielBrandao12 <drb12da@gmail.com>
2023-01-26 14:59:01 -0300

    Criação de carrinho de compra

--- public/javascripts/addCart.js
+++ public/javascripts/addCart.js
@@ -7,12 +7,12 @@ const btnAddCart = document.querySelectorAll('#btn-add-cart')
 const carItens = document.querySelector('.cart-itens')
 const containerCart = document.querySelector('.container-cart-books')
 const cardProduct = document.querySelectorAll('.card-product')
-const title = document.querySelectorAll('.title-prod')
 
 
 
 
-if(carItens.innerText == ''){
+
+if (carItens.innerText == '') {
     carItens.style.color = '#23232E'
     carItens.style.background = '#23232E'
 }
@@ -23,46 +23,46 @@ let listCart = []
 //if verifica se existe alguma coisa dentro do localStorage listCar,
 //se estiver vazio listCart recebe [vazio]
 //senão recebe todos os itens o storage e manten os arquivos salvos no carrinho
-if(!localStorage.getItem('listCart')){
-     listCart = []
-}else{
+if (!localStorage.getItem('listCart')) {
+    listCart = []
+} else {
 
     console.log()
-     JSON.parse(localStorage.getItem('listCart')).forEach(e=>{
+    JSON.parse(localStorage.getItem('listCart')).forEach(e => {
         listCart.unshift(e)
     })
 }
 
 
 
-btnAddCart.forEach(btn =>{
-    
-    btn.addEventListener('click', e=>{
+btnAddCart.forEach(btn => {
 
-      
-    
-        cardProduct.forEach(card =>{
-            if(btn.value == card.id){
-           
+    btn.addEventListener('click', e => {
 
-                
-            console.log(listCart)
-                listCart.push({"img":card.children[0].src, "title": card.children[1].innerText, "preco":card.children[2].innerText})
-                
-                
-                    let itens = JSON.stringify(listCart)
-                    localStorage.setItem('listCart', itens)
-                    
-                
 
-                    console.log(listCart)
-              
+
+        cardProduct.forEach(card => {
+            if (btn.value == card.id) {
+
+
+
+                console.log(listCart)
+                listCart.push({ "img": card.children[0].src, "title": card.children[1].innerText, "preco": card.children[2].innerText })
+
+
+                let itens = JSON.stringify(listCart)
+                localStorage.setItem('listCart', itens)
+
+
+
+                console.log(listCart)
+
 
                 let localCard = JSON.parse(localStorage.getItem(`listCart`))
 
-                let i =localCard.length-1
-                if(localCard.length==0){
-                    i =0
+                let i = localCard.length - 1
+                if (localCard.length == 0) {
+                    i = 0
                 }
                 console.log(i)
                 let div = document.createElement('div')
@@ -80,33 +80,33 @@ btnAddCart.forEach(btn =>{
              </div>
            
                 `
-                
+
                 containerCart.appendChild(div)
-                
-                 
+
+
                 i++
                 carItens.innerText = localCard.length
             }
         })
- 
-         carItens.style.color = '#ffffff'
+
+        carItens.style.color = '#ffffff'
         carItens.style.background = '#1C98ED'
     })
 })
 
 
 
-window.addEventListener('load', ()=>{
+window.addEventListener('load', () => {
 
-    
+
 
     let localCard = JSON.parse(localStorage.getItem(`listCart`))
     console.log(localCard[0].img)
-  
-        for (let j= 0; j<localCard.length; j++){
-   
-            let div = document.createElement('div')
-                div.innerHTML = `
+
+    for (let j = 0; j < localCard.length; j++) {
+
+        let div = document.createElement('div')
+        div.innerHTML = `
               
                 <div class="books-cart">
                 <img src="${localCard[j].img}" alt="">
@@ -120,10 +120,10 @@ window.addEventListener('load', ()=>{
              </div>
            
                 `
-                
-                containerCart.appendChild(div)
-        }
-        carItens.style.color = '#ffffff'
-        carItens.style.background = '#1C98ED'
-        carItens.innerText = localCard.length
+
+        containerCart.appendChild(div)
+    }
+    carItens.style.color = '#ffffff'
+    carItens.style.background = '#1C98ED'
+    carItens.innerText = localCard.length
 })
