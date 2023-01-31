1dde5b10d94fe474b3437a83448150377c6a2ab9
DanielBrandao12 <drb12da@gmail.com>
2023-01-31 17:25:18 -0300

    input para contagem de itens no carrinho

--- public/javascripts/addCart.js
+++ public/javascripts/addCart.js
@@ -1,9 +1,4 @@
-
-
-
-
 const btnAddCart = document.querySelectorAll('#btn-add-cart')
-
 const carItens = document.querySelector('.cart-itens')
 const containerCart = document.querySelector('.container-cart-books')
 const cardProduct = document.querySelectorAll('.card-product')
@@ -47,11 +42,9 @@ btnAddCart.forEach(btn => {
 
 
                 console.log(listCart)
-                listCart.push({ "img": card.children[0].src, "title": card.children[1].innerText, "preco": card.children[2].innerText })
+                listCart.push({ "img": card.children[0].src, "title": card.children[1].innerText, "qtde": 1, "preco": card.children[2].innerText })
 
-
-                let itens = JSON.stringify(listCart)
-                localStorage.setItem('listCart', itens)
+                addStorage(listCart)
 
 
 
@@ -64,7 +57,7 @@ btnAddCart.forEach(btn => {
                 if (localCard.length == 0) {
                     i = 0
                 }
-                console.log(i)
+
                 let div = document.createElement('div')
                 div.innerHTML = `
               
@@ -73,8 +66,15 @@ btnAddCart.forEach(btn => {
                 <div class="container-titles">
                    <strong class="title-cart"> ${localCard[i].title}</strong>
                    <div class="container-strong">
-                      <strong>Quantidade: 1</strong>
-                      <strong>Valor:  ${localCard[i].preco}</strong>
+                   <span class="span-qtde-cart">
+                        <button id="${i}"  class="btn-cart-min">-</button>
+                        <input type="text" id="${i}" value="${localCard[i].qtde}" class="input-qtde-cart">
+                        <button id="${i}"  class="btn-cart-max">+</button>
+                    </span>
+                    <strong >
+                    <strong >Valor:</strong>
+                      <strong class="strong-cart-price">${localCard[i].preco}</strong>
+                      </strong>
                    </div>
                 </div>
              </div>
@@ -88,7 +88,8 @@ btnAddCart.forEach(btn => {
                 carItens.innerText = localCard.length
             }
         })
-
+        somaQtdeCart()
+        subQtdeCart()
         carItens.style.color = '#ffffff'
         carItens.style.background = '#1C98ED'
     })
@@ -101,7 +102,7 @@ window.addEventListener('load', () => {
 
 
     let localCard = JSON.parse(localStorage.getItem(`listCart`))
-    console.log(localCard[0].img)
+
 
     for (let j = 0; j < localCard.length; j++) {
 
@@ -113,17 +114,99 @@ window.addEventListener('load', () => {
                 <div class="container-titles">
                    <strong class="title-cart"> ${localCard[j].title}</strong>
                    <div class="container-strong">
-                      <strong>Quantidade: 1</strong>
-                      <strong>Valor:  ${localCard[j].preco}</strong>
-                   </div>
+                   <span class="span-qtde-cart">
+                        <button id="${j}" class="btn-cart-min">-</button>
+                        <input type="text" id="${j}" value="${localCard[j].qtde}" class="input-qtde-cart">
+                        <button id="${j}" class="btn-cart-max">+</button>
+                    </span>
+                    <strong >
+                    <strong >Valor:</strong>
+                    <strong class="strong-cart-price">${localCard[j].preco}</strong>
+                    </strong>
+                    </div>
                 </div>
              </div>
            
                 `
 
         containerCart.appendChild(div)
+
     }
+
+    somaQtdeCart()
+    subQtdeCart()
+
+  
+
     carItens.style.color = '#ffffff'
     carItens.style.background = '#1C98ED'
     carItens.innerText = localCard.length
+
+
 })
+
+//função vai somar a quantidade de itens de um item adicionado no carrinho
+//vai mudar o valor do preço conforme aumentar a quantidade
+//e aumentar o numero do input
+function somaQtdeCart() {
+    let localCard = JSON.parse(localStorage.getItem(`listCart`))
+    const btnMax = document.querySelectorAll('.btn-cart-max')
+    const inputQtdeCart = document.querySelectorAll('.input-qtde-cart')
+    const precoCart = document.querySelectorAll('.strong-cart-price')
+    
+
+    btnMax.forEach(btnM => {
+        btnM.addEventListener('click', () => {
+
+            inputQtdeCart[btnM.id].value = parseInt(inputQtdeCart[btnM.id].value) + 1
+
+            let valor = parseFloat(localCard[btnM.id].preco.replace(/[^0-9,]*/g, '').replace(',', '.')) * parseFloat(inputQtdeCart[btnM.id].value)
+
+            precoCart[btnM.id].innerText = valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+            listCart[btnM.id].preco = precoCart[btnM.id].innerText
+            listCart[btnM.id].qtde = inputQtdeCart[btnM.id].value
+            console.log(listCart)
+            addStorage(listCart)
+        })
+    })
+}
+function subQtdeCart() {
+    let localCard = JSON.parse(localStorage.getItem(`listCart`))
+    const btnMin = document.querySelectorAll('.btn-cart-min')
+    const inputQtdeCart = document.querySelectorAll('.input-qtde-cart')
+    const precoCart = document.querySelectorAll('.strong-cart-price')
+    const booksCart = document.querySelectorAll('.books-cart')
+
+    btnMin.forEach(btnM => {
+        btnM.addEventListener('click', () => {
+           
+            inputQtdeCart[btnM.id].value = parseInt(inputQtdeCart[btnM.id].value) - 1
+
+            let valor = parseFloat(localCard[btnM.id].preco.replace(/[^0-9,]*/g, '').replace(',', '.')) * parseFloat(inputQtdeCart[btnM.id].value)
+
+            precoCart[btnM.id].innerText = valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+            listCart[btnM.id].preco = precoCart[btnM.id].innerText
+            listCart[btnM.id].qtde = inputQtdeCart[btnM.id].value
+           
+            addStorage(listCart)
+
+            //resolver isso depois
+            if( parseInt(inputQtdeCart[btnM.id].value) <= 0){
+                localStorage.removeItem(listCart[btnM.id])
+                console.log(parseInt(inputQtdeCart[btnM.id].value))
+                console.log(btnM.id)
+                
+            }
+        })
+    })
+}
+
+
+
+//função para adicionar valor ao storage
+function addStorage(valor) {
+
+    let itens = JSON.stringify(valor)
+    localStorage.setItem('listCart', itens)
+
+}
