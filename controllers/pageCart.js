5e929405c59c3a66f7b19d63de0a18dd276de9e8
DanielBrandao12 <drb12da@gmail.com>
2023-02-11 14:46:27 -0300

    atualização

--- controllers/pageCart.js
+++ controllers/pageCart.js
@@ -1,9 +1,24 @@
 
+const database = require('../database/models');
 
 function pageCart(req, res) {
+  let id = req.session.userLogged.id
+  database.Address.findAll({
+    where: {
+      USERS_ID: id
+    }
+  }).then((data) => {
+    let myAddress = data
+
+
+ res.render('pageCart', {
+
+    userLogged: req.session.userLogged,
+    myAddress
+  })
+  });
   
-  
-  res.render('pageCart')
+
 
 };
 
--- middlewares/loggedUserDataMiddleware.js
+++ middlewares/loggedUserDataMiddleware.js
@@ -1,4 +1,9 @@
 function loggedUserDataMiddleware(req, res , next){
+
+
+    //Verifica se tem um usuário logado na sessão
+    //Se sim define a propriedade res.locals.isLogged como true
+
     res.locals.isLogged = false
    
     if(req.session.userLogged){
